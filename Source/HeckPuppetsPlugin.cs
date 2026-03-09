1d70fc0d2952656f02f17c8229578c5bff6c2904
Nixya <64989593+nyxpirientity@users.noreply.github.com>
2026-03-08 23:07:41 -0700

    Initial Commit

---
+++ Source/HeckPuppetsPlugin.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using BepInEx;
+
+namespace Nyxpiri.ULTRAKILL.HeckPuppets
+{
+    [BepInPlugin("com.nyxpiri.bepinex.plugins.ultrakill.heck-puppets", "Heck Puppets", "0.0.0.1")]
+    [BepInProcess("ULTRAKILL.exe")]
+    public class HeckPuppetsPlugin : BaseUnityPlugin
+    {
+        protected void Awake()
+        {
+            Log.Initialize(Logger);
+        }
+
+        protected void Start()
+        {
+        }
+
+        protected void Update()
+        {
+
+        }
+
+        protected void LateUpdate()
+        {
+
+        }
+    }
+}
